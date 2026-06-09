8cee5502fc6a713981d49898fecfe6e70d6be2aa
somjeet <somjeetkumar30@gmail.com>
2026-06-09 18:01:08 +0530

    add cloudinary

--- StudyPlatForm/settings.py
+++ StudyPlatForm/settings.py
@@ -82,10 +82,24 @@ INSTALLED_APPS = [
     'Auth_system',
     'corsheaders',
     'rest_framework.authtoken',
-    'project'
+    'project',
+    "cloudinary",
+    "cloudinary_storage",
 
 
 ]
+
+
+
+
+
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
+}
+
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 CORS_ALLOW_ALL_ORIGINS = True
 
